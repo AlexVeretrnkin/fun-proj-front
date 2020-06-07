fc11de16caae206bce27173aab753d725f331455
AlexVeretenkin <alexveretenkin@gmail.com>
2020-06-07 21:41:07 +0300

    added animation for description

--- src/ongoing/ongoing-item/ongoing-item.tsx
+++ src/ongoing/ongoing-item/ongoing-item.tsx
@@ -1,34 +1,33 @@
-import { Card } from '@material-ui/core';
+import {Card} from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import CardMedia from "@material-ui/core/CardMedia";
 import React from 'react';
 
 import './ongoing-item.scss';
 
 export default function OngoingItem() {
-  return (
-    <Card className='card'>
-      <CardContent>
-        <Typography color="textSecondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="h2">
-          sd
-        </Typography>
-        <Typography color="textSecondary">
-          adjective
-        </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-  );
+    return (
+        <Card className='card'>
+            <CardMedia
+                className='card__media'
+                image="https://static.anilibria.tv/upload/release/270x390/7439.jpg?1566259183"
+            />
+            <CardContent className='card__content'>
+                <Typography className='card__text card__text_title' color="textPrimary" variant='h6' gutterBottom>
+                    Мастера Меча Онлайн: Алисизация
+                    <Typography className='card__text' component='p' color="textSecondary">
+                        Серии 36
+                    </Typography>
+                </Typography>
+
+                <Typography className='card__text card__text_description' color="textSecondary">
+                    Underworld - мир, ранее недоступный человеческому пониманию. Мир, со своими законами - "Индексом
+                    Запретов". За его соблюдением следит "Церковь Аксиомы" и рыцари единства, стать...
+                </Typography>
+            </CardContent>
+        </Card>
+    );
 }
